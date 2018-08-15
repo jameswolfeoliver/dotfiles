15a1514f0c737547e41cc29ce6ae2fc427f373fa
jameswolfeoliver <jamesoliver88@gmail.com>
2018-08-15 13:49:17 -0400

    WIP

--- install.sh
+++ install.sh
@@ -49,9 +49,11 @@ echo "\n\nInstalling powerlevel9k"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 echo "\n\nInstalling powerlevel9k fonts"
-curl https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf > ~/.local/share/fonts/
+curl https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf > ~/.local/share/fonts/PowerlineSymbols.otf
+curl https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf > ~/.config/fontconfig/conf.d/10-powerline-symbols.conf
+
 fc-cache -vf ~/.local/share/fonts/
-curl https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf > ~/.config/fontconfig/conf.d/
+
 
 echo "\n\nInstalling Vim Plug"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
